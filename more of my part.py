6e0f23b2a1113dd4ef6f6194070596eac278d70d
MarkMahoneyRush <16MMahoney@stjosephsrush.ie>
2018-05-21 17:05:32 +0100

    Create more of my part.py

---
+++ more of my part.py
@@ -0,0 +1,203 @@
+from time import sleep #Mark
+while True: #Marks code
+    
+
+    print("This is the division section. Answer very carefully as you are nearly out of the calculator.") #Mark
+    sleep(5) #Mark
+    print("If you answer the question wrong, you will be transported to the start of the calculator") #Mark
+    sleep(5) #Mark
+    print("This level will be your hardest challenge yet.") #Mark
+    sleep(5)
+    print("there will be 19 questions in this section so prepare yourself mentally")
+    sleep(5) 
+    print("Nobody has ever got this far before")
+    sleep(4)
+    print("you have been my hardest challenge yet")
+    sleep(5)
+    print("but the question is...")
+    sleep(5)
+    print("Are you ready...")
+    sleep(5)
+    print("For the hardest piece of division you are ever going to see!!?!?!?")
+    sleep(5)
+    print("lets find out...")
+    sleep(5) #Mark
+    print("Get ready..")
+    sleep(3)
+    print("3")
+    sleep(1)
+    print("2")
+    sleep(1)
+    print("1")
+    while True:
+        choice=input("What is 90 divided by 9?") #Mark
+        if choice == "10": #mark
+            print("correct! You move on!") #mark
+            break #mark
+        else: #mark
+            print("incorrect!") #mark
+    
+    print("here is question 2! Get ready!") #mark
+    while True:#mark
+        choice=input("what is 72 divided by 8?") #mark
+        if choice == "9": #mark
+            print("You have fooled the calculator again! you move on!") #mark
+            break
+        else: #mark
+            print("Wrong!") #mark
+        
+    print("You are now on question 3! Time is ticking!") #mark
+    while True: #mark
+        choice=input("what is 250 divided by 5!")#mark
+        if choice == "50":#mark
+            print("Correct answer. You are nearly out of the calculator")#mark
+            break
+        else:#mark
+            print("incorrect!")#mark
+            
+    print("You are now on question 4!") #mark
+    while True: #mark
+        choice=input("what is 99 divided by 3!")#mark
+        if choice == "33":#mark
+            print("Correct answer.")#mark
+            break
+        else:#mark
+            print("incorrect!")#mark
+            
+    print("Question 5.") #mark
+    while True: #mark
+        choice=input("What is 180 divided by 6!?")#mark
+        if choice == "30":#mark
+            print("Correct answer.")#mark
+            break
+        else:#mark
+            print("incorrect!")#mark
+            
+    print("Question 6.")
+    while True:
+        choice=input("80 divided by 5!?") #Mark
+        if choice == "16": #mark
+            print("correct! You move on!") #mark
+            break #mark
+        else: #mark
+            print("incorrect!") #mark
+    
+    print("Question 7.") #mark
+    while True:#mark
+        choice=input("63 divided by 7!?") #mark
+        if choice == "9": #mark
+            print("You have fooled the calculator again! you move on!") #mark
+            break
+        else: #mark
+            print("Wrong!") #mark
+        
+    print("Question 8.") #mark
+    while True: #mark
+        choice=input("88 divided by 11!? ")#mark
+        if choice == "8":#mark
+            print("Correct answer.")#mark
+            break
+        else:#mark
+            print("incorrect!")#mark
+            
+    print("Question 9.") #mark
+    while True: #mark
+        choice=input("45 divided by 9")#mark
+        if choice == "5":#mark
+            print("Correct answer.")#mark
+            break
+        else:#mark
+            print("incorrect!")#mark
+            
+    print("Question 10.") #mark
+    while True: #mark
+        choice=input("144 divided by 2!?")#mark
+        if choice == "77":#mark
+            print("Correct answer.")#mark
+            break
+        else:#mark
+            print("incorrect!")#mark
+            
+    
+    print("Question 11.")
+    while True:
+        choice=input("84 divided by 7!?") #Mark
+        if choice == "12": #mark
+            print("correct! You move on!") #mark
+            break #mark
+        else: #mark
+            print("incorrect!") #mark
+    
+    print("Question 12.") #mark
+    while True:#mark
+        choice=input("92 divided by 4?!") #mark
+        if choice == "23": #mark
+            print("You have fooled the calculator again! you move on!") #mark
+            break
+        else: #mark
+            print("Wrong!") #mark
+        
+    print(" Question 13.Time is ticking!") #mark
+    while True: #mark
+        choice=input("108 divided by 9!? ")#mark
+        if choice == "12":#mark
+            print("Correct answer.")#mark
+            break
+        else:#mark
+            print("incorrect!")#mark
+            
+    print("Question 14.") #mark
+    while True: #mark
+        choice=input("180 divided by 9")#mark
+        if choice == "20":#mark
+            print("Correct answer.")#mark
+            break
+        else:#mark
+            print("incorrect!")#mark
+            
+    print("Question 15.") #mark
+    while True: #mark
+        choice=input("98 divided by 2!?")#mark
+        if choice == "49":#mark
+            print("Correct.")#mark
+            break
+        else:#mark
+            print("incorrect!")#mark
+            
+    print("Question 1.") #mark
+    while True: #mark
+        choice=input("60 divided by 4!?")#mark
+        if choice == "15":#mark
+            print("Correct.")#mark
+            break
+        else:#mark
+            print ("incorrect!")#mark
+            
+    print("Question 17.") #mark
+    while True: #mark
+        choice=input("1000 divided by 10")#mark
+        if choice == "100":#mark
+            print("Correct.")#mark
+            break
+        else:#mark
+            print("incorrect!")#mark
+            
+            
+    print("Question 18. The secondlast question, the pressure is on") #mark
+    while True: #mark
+        choice=input("10,000 divided by 10?")#mark
+        if choice == "1000":#mark
+            print("Correct.")#mark
+            break
+        else:#mark
+            print("incorrect!")#mark
+            
+    print("Question 19. The final question...") #mark
+    while True: #mark
+        choice=input("1,000,000 divided by... 100..")#mark
+        if choice == "10,000":#mark
+            print("Correct. I have been defeated")#mark
+            breakak
+        else:#mark
+            print("incorrect!")#mark
+            
